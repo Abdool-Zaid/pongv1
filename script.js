13f7b5d82e344e911ed2dcf2ceac5e4cc4ec5d4e
Abdool-Zaid <aztoorabally7447@gmail.com>
2023-07-17 14:45:50 +0200

    'scene'

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log("working")
+ // Create a scene
+ const scene = new THREE.Scene();
+
+ // Create a cube
+ const geometry = new THREE.BoxGeometry(1, 1, 1);
+ const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+ const cube = new THREE.Mesh(geometry, material);
+ scene.add(cube);
+
+ // Create a camera
+ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+ camera.position.z = 5;
+
+ // Create a renderer
+ const renderer = new THREE.WebGLRenderer();
+ renderer.setSize(window.innerWidth, window.innerHeight);
+ document.body.appendChild(renderer.domElement);
+
+ // Render the scene
+ function animate() {
+   requestAnimationFrame(animate);
+   cube.rotation.x += 0.01;
+   cube.rotation.y += 0.01;
+   renderer.render(scene, camera);
+ }
+ animate();
